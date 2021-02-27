1a08eced64b3d39c215027a0f67993c78d5c2300
Dang Van Thanh <dangvanthanh@dangthanh.org>
2021-02-27 22:00:42 +0700

    Add empty dist

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import esbuild from 'rollup-plugin-esbuild'
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
+import { emptyDir } from 'rollup-plugin-empty-dir'
 import zip from 'rollup-plugin-zip'
 
 const production = !process.env.ROLLUP_WATCH
@@ -20,6 +21,7 @@ export default {
     }),
     commonjs(),
     esbuild({}),
+    emptyDir(),
     production && zip({ dir: 'releases' }),
   ],
 }
